b37411c6a6492118d64e72bc6b121cf5bc4e68f6
Vins2001 <leniirawati01@gmail.com>
2022-03-12 16:41:18 +0700

    Initial commit

---
+++ app/src/main/java/com/leni192102212/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.leni192102212.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
